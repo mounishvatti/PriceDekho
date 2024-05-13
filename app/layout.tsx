5b5956dc6858908820f93746d711a7e2652ebcc7
mounishvatti <mounishvatti2002@gmail.com>
2024-05-14 00:48:07 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const space_Grotesk = Space_Grotesk({
+  subsets:["latin"], 
+  weight: ['300','400','500','600','700']
+});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
