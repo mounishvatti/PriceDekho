440135ad68f367e121645978767fa53884a9626d
mounishvatti <mounishvatti2002@gmail.com>
2024-05-16 02:15:47 +0530

    added landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,50 @@
+import { Searchbar } from '@/components/Searchbar'
 import React from 'react'
+import Image from 'next/image'
+import HeroCarousel from '@/components/HeroCarousel'
 
-const home = () => {
+const Home = () => {
   return (
-    <div>home</div>
+    <>
+      <section className='px-6 md:px-20 py-24'>
+        <div className='flex max-xl:flex-col gap-16'>
+          <div className='flex flex-col justify-center'>
+            <p className='small-text'>
+              Smart Shopping Starts Here:
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+            <h1 className='head-text'>
+              Unleash the Power of
+              <span className='text-primary'> PriceDekho</span>
+            </h1>
+            <p className='mt-6'>
+              Powerful, self-serve product and growth analytics to help you convert, engage and retain more.
+            </p>
+            
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className='trending-section'>
+        <h2 className='section-text'>Trending</h2>
+
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+          {["Apple iPhone 15", "Book", "Sneakers"].map
+          ((product) => (
+            <div>{product}</div>
+          ))}
+        </div>
+      </section>
+    </>
   )
 }
 
-export default home
+export default Home
